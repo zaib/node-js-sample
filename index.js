8410350205058c84623135f9dd6879421c70c3ab
Jahanzaib_Aslam <jahanzaib_aslam@epam.com>
2017-04-15 13:59:24 -0400

    default port 80

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+console.log(process.env.PORT);
+
+app.set('port', (80))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
