f13a1c64325f732325c75af658e5e1925545146d
Jahanzaib_Aslam <jahanzaib_aslam@epam.com>
2017-04-15 14:42:28 -0400

    log env port

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var express = require('express')
 var app = express()
 
-app.set('port', 80)
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.listen(80, function() {
+app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-  console.log(process.env.PORT);
 })
