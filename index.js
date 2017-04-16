79ac6ffcca479be40db8783e9a541f9830684be0
Jahanzaib_Aslam <jahanzaib_aslam@epam.com>
2017-04-15 21:05:11 -0400

    playing with port

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.listen(80, function() {
+app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
